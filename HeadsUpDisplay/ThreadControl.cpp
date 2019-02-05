59b027315b4397e7e77072c188d6562b49d9e6fd
jeevanprakash0814 <jeevanpraks@gmail.com>
2019-02-05 15:49:50 -0800

    Added Two New Files

---
+++ HeadsUpDisplay/ThreadControl.cpp
@@ -0,0 +1,9 @@
+//
+//  ThreadControl.cpp
+//  HeadsUpDisplay
+//
+//  Created by Jeevan Prakash on 2/5/19.
+//  Copyright © 2019 Jeevan Prakash. All rights reserved.
+//
+
+#include "ThreadControl.hpp"
---
+++ HeadsUpDisplay/ThreadControl.hpp
@@ -0,0 +1,14 @@
+//
+//  ThreadControl.hpp
+//  HeadsUpDisplay
+//
+//  Created by Jeevan Prakash on 2/5/19.
+//  Copyright © 2019 Jeevan Prakash. All rights reserved.
+//
+
+#ifndef ThreadControl_hpp
+#define ThreadControl_hpp
+
+#include <stdio.h>
+
+#endif /* ThreadControl_hpp */
