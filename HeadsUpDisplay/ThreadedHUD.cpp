ec53d9c5b238b31b82c41e906a06a900b9a22d7a
jeevanprakash0814 <jeevanpraks@gmail.com>
2019-02-10 17:32:50 -0800

    Removing uneccesary files

--- HeadsUpDisplay/ThreadedHUD.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  ThreadControl.cpp
-//  HeadsUpDisplay
-//
-//  Created by Jeevan Prakash on 2/5/19.
-//  Copyright © 2019 Jeevan Prakash. All rights reserved.
-//
-
-#include "ThreadControl.hpp"
